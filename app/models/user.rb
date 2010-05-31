4f59749015c5279975837487ac6c0eac8327451e
Aditya Dixit <adityadixit06@gmail.com>
2010-06-01 00:43:16 +0530

    Done with sign in

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 # == Schema Information
-# Schema version: 20100530100021
+# Schema version: 20100531152347
 #
 # Table name: users
 #
@@ -10,6 +10,7 @@
 #  updated_at         :datetime
 #  encrypted_password :string(255)
 #  salt               :string(255)
+#  remember_token     :string(255)
 #
 
 # == Schema Information
